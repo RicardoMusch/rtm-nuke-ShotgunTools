0de797f562c4197e67fd8878c9d71779d648ec1e
Ricardo Musch <ricardom@lola-post.com>
2019-09-20 12:47:12 +0100

    Create sg_connection_example.py

---
+++ sg_connection_example.py
@@ -0,0 +1,8 @@
+#############################################################
+## Example sg_connection.py file
+"Fill in the data below and rename this file sg_connection.py"
+#############################################################
+
+SERVER_PATH = "https://YOURSTUDIO.shotgunstudio.com"
+SCRIPT_NAME = 'YOUR_SCRIPT_NAME'
+SCRIPT_KEY = 'XYXYXYXYXYYXYXYXYXYXYX'
