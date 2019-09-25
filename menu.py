14ba90b0730b882434261c716b2d389521aa42b1
NodesLayers <ricardo@nodeslayers.com>
2019-09-25 01:06:13 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ print " "
 print "Loading getShotgunData gizmo Menu"
 ##########################################
 
+import nuke
 toolbar = nuke.toolbar("Nodes")
 #toolbar.addCommand( "Shotgun/NukeTools", "nuke.createNode('MyGizmo')")
 toolbar.addCommand("Shotgun/NukeTools/getShotgunData", "nuke.createNode(\"getShotgunData.gizmo\")", icon="Text.png")
